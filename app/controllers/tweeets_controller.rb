a59cb013b959ae2d846e5bc9050bc92f63d37493
OdongAlican <sandieo.2020@gmail.com>
2020-06-04 18:35:21 +0300

    adds the option to edit

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,5 +1,6 @@
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /tweeets
   # GET /tweeets.json
@@ -14,7 +15,7 @@ class TweeetsController < ApplicationController
 
   # GET /tweeets/new
   def new
-    @tweeet = Tweeet.new
+    @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
@@ -23,7 +24,7 @@ class TweeetsController < ApplicationController
   # POST /tweeets
   # POST /tweeets.json
   def create
-    @tweeet = Tweeet.new(tweeet_params)
+    @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
